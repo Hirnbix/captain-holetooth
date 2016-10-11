857fd73d5d8a7394aa5c53d77530b9e2e5b11aea
Oliver Ruehl <ruehl@serpens.uberspace.de>
2016-10-11 17:09:27 +0200

    Menu work

--- scenes/scn1-menu/menu.gd
+++ scenes/scn1-menu/menu.gd
@@ -5,11 +5,21 @@ extends Node2D
 # var a=2
 # var b="textvar"
 
+func menu_reload():
+	get_tree().reload_current_scene()
+
 func _ready():
 	for scene in game.opened_scenes:
 		if has_node("jump_" + scene):
 			get_node("jump_" + scene).show()
-			
+	#TranslationServer.set_locale("en")
+	
+	get_node("menu_buttons/startbutton").set_text(tr("KEY_START"))
+	get_node("menu_buttons/optionsbutton").set_text(tr("KEY_OPTIONS"))
+	get_node("options_screen/settings/General").set_tab_title(tr("KEY_GENERAL_INFO"))
+	get_node("options_screen/settings/Debug").set_tab_title(tr("KEY_DEBUG"))
+	get_node("options_screen/settings/Debug").set_tab_title("fsefs")
+	#get_node("art/bg/gametitle").set_texture("res://scenes/scn1-menu/scn1_menu_gametitle_EN.png")
 	
 func _on_jump_scn3_pressed():
 	print("Going to scn3")
@@ -26,3 +36,25 @@ func _on_jump_scn5_pressed():
 func _on_jump_minigame_pressed():
 	print("Going to minigame")
 	transition.fade_to("res://scenes/yan-kandy-minigame/prototype.tscn")
+
+func _on_startbutton_pressed():
+	transition.fade_to("res://scenes/scn2-comic-intro/scn2.tscn")
+
+func _on_optionsbutton_pressed():
+	get_node("options_screen").show()
+
+func _on_exitbutton_pressed():
+	get_tree().quit()
+
+func _on_de_button_pressed():
+	TranslationServer.set_locale("de")
+	#get_node("art/bg/gametitle").set_texture("res://scenes/scn1-menu/scn1_menu_gametitle_DE.png")
+	menu_reload()
+
+func _on_en_button_pressed():
+	TranslationServer.set_locale("en")
+	#get_node("art/bg/gametitle").set_texture("scn1_menu_gametitle_EN.png")
+	menu_reload()
+
+func _on_btn_close_options_pressed():
+	get_node("options_screen").hide()
