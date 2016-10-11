f81695dc49fdc085de3f9f8f9168b3aca4cee836
Oliver Ruehl <ruehl@serpens.uberspace.de>
2016-10-11 23:44:55 +0200

    Localization

--- scenes/scn1-menu/menu.gd
+++ scenes/scn1-menu/menu.gd
@@ -1,25 +1,28 @@
 
-extends Node2D
+extends Control
 
 # member variables here, example:
 # var a=2
 # var b="textvar"
 
+
+
 func menu_reload():
-	get_tree().reload_current_scene()
+	var s = load("res://scenes/scn1-menu/scn1.tscn")
+	var si = s.instance()
+	get_parent().add_child(si)
+	queue_free()
 
 func _ready():
 	for scene in game.opened_scenes:
 		if has_node("jump_" + scene):
 			get_node("jump_" + scene).show()
-	#TranslationServer.set_locale("en")
 	
 	get_node("menu_buttons/startbutton").set_text(tr("KEY_START"))
 	get_node("menu_buttons/optionsbutton").set_text(tr("KEY_OPTIONS"))
 	get_node("options_screen/settings/General").set_tab_title(tr("KEY_GENERAL_INFO"))
 	get_node("options_screen/settings/Debug").set_tab_title(tr("KEY_DEBUG"))
-	get_node("options_screen/settings/Debug").set_tab_title("fsefs")
-	#get_node("art/bg/gametitle").set_texture("res://scenes/scn1-menu/scn1_menu_gametitle_EN.png")
+
 	
 func _on_jump_scn3_pressed():
 	print("Going to scn3")
@@ -47,13 +50,11 @@ func _on_exitbutton_pressed():
 	get_tree().quit()
 
 func _on_de_button_pressed():
-	TranslationServer.set_locale("de")
-	#get_node("art/bg/gametitle").set_texture("res://scenes/scn1-menu/scn1_menu_gametitle_DE.png")
+	TranslationServer.set_locale("de_DE")
 	menu_reload()
 
 func _on_en_button_pressed():
-	TranslationServer.set_locale("en")
-	#get_node("art/bg/gametitle").set_texture("scn1_menu_gametitle_EN.png")
+	TranslationServer.set_locale("en_GB")
 	menu_reload()
 
 func _on_btn_close_options_pressed():
