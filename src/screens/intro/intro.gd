7ccf56808cb2a35ed98432a34c174ff7184e100e
Tybobobo <tybobobo@protonmail.ch>
2016-10-18 14:02:07 +0200

    Cleaned Main Menu. Cleaned Forest level folder structure. Moved music player inside audio folder inside its on folder along with its own script.

--- src/screens/intro/intro.gd
+++ src/screens/intro/intro.gd
@@ -1,4 +1,6 @@
 extends TextureFrame
 
+# Mouse click on screen to continue
 func _on_skipbutton_pressed():
+	# Transition to Scene 3 (Forest)
 	transition.fade_to("res://src/levels/forest/forest.tscn")
