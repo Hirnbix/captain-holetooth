978cb76e7ae3c12da0d04502729cb0c0f418b5f0
Tybobobo <tybobobo@protonmail.ch>
2016-10-25 14:32:27 +0200

    Fixed issue where the intro cutscene did not automatically continue at the end of it

--- src/screens/intro/intro.gd
+++ src/screens/intro/intro.gd
@@ -1,6 +1,12 @@
 extends TextureFrame
 
+# Level path to the scene we are switching to
+var level_path = "res://src/levels/forest/forest.tscn"
+
 # Mouse click on screen to continue
 func _on_skipbutton_pressed():
-	# Transition to Scene 3 (Forest)
-	transition.fade_to("res://src/levels/forest/forest.tscn")
+	transition.fade_to(level_path)
+
+# On the completion of the animation, continue to the game
+func _on_animation_player_finished():
+	transition.fade_to(level_path)
