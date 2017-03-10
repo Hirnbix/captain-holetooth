130cf701be3e2f258ea86733408dcab7dd37ff5e
Oliver Ruehl <oliverruehl@mail.de>
2017-03-10 06:50:53 +0100

    Character card HUD animation

--- src/actors/yan/yan_card_unlock.gd
+++ src/actors/yan/yan_card_unlock.gd
@@ -5,9 +5,11 @@ extends TextureFrame
 # var b = "textvar"
 
 onready var animations = get_node("animation")
+onready var yan = get_node("/root/scn3/world/NPCs/Yan")
 
 func _ready():
-		if global.characters_met.has("Yan"):
+	yan.connect("met_yan",self,"_on_met_yan")
+
+func _on_met_yan():
+			get_node("sfx").play("card_unlock")
 			animations.play("yan_unlock_anim")
-		else:
-			print("yan not found")
