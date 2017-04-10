21a6349a49ea379e14535080621d9c1bed4f536d
Oliver Ruehl <oliverruehl@mail.de>
2017-04-10 10:26:11 +0200

    Character cards button functionality

--- src/screens/character-cards/character-cards.gd
+++ src/screens/character-cards/character-cards.gd
@@ -4,7 +4,7 @@ extends Control
 # var a = 2
 # var b = "textvar"
 
-onready var anim = get_node("AnimationPlayer")
+
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -15,7 +15,3 @@ func _ready():
 func _on_TextureButton_pressed():
 	print("Debug: Jumping to Menu")
 	transition.fade_to("res://src/screens/menu/menu.tscn")
-
-
-func _on_card_button_pressed():
- 	anim.play("flip_card")
