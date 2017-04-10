2b04ab254d34c97eb9c149114cff262464bdfc60
Oliver Ruehl <oliverruehl@mail.de>
2017-04-10 10:25:24 +0200

    Fixed character card pop up Yan

--- src/screens/hud/hud.gd
+++ src/screens/hud/hud.gd
@@ -14,10 +14,7 @@ func _on_met_yan():
 
 func _ready():
 	if yan && global.last_pos[0] == Vector2(0,0):
-		print("Yan is present")
 		yan.connect("met_yan", self,"_on_met_yan")
-		animations.play("yan_unlock_anim")
-		print("working")
 	set_process_input(true)
 	update_scores()
 	game.connect("scores_changed", self, "update_scores")
