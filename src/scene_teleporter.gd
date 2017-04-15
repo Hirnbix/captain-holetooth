5c9cbdfaa992c396f6c7196f974c8a7f391ff1ac
Oliver Ruehl <oliverruehl@mail.de>
2017-04-15 16:53:31 +0200

    Level updates

--- src/scene_teleporter.gd
+++ src/scene_teleporter.gd
@@ -14,12 +14,13 @@ func _on_scene_teleporter_body_enter( body ):
 		print("You have the wing")
 	else:
 		print("Sorry, no wing")
-	
+	body.set_process(false)
 	#find_node("teleporter_debug_label").set_text(scene_path)
 	if body.get_name() == "player":
 		var current = get_tree().get_current_scene().get_name()
 		global.last_pos[current[current.length()-1].to_int() - 3] = body.get_global_pos()
 		transition.fade_to(str(scene_path))
+
 	#transition.fade_to("res://" + scene_path)
 	#var tele1pos = get_node("/root/scn3/tele1/teleporter1").get_global_pos()
 	#var tele2pos = self.get_pos()
