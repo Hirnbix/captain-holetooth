8c2bb57abf80b39d45de9db5e8818a879e9c6812
Oliver Ruehl <oliverruehl@mail.de>
2017-07-28 22:01:48 +0200

    Removed rope completely
    It was just too annoying to get past.

--- addons/Rope/curve_editor.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends Node2D
-
-var plugin
-
-func _draw():
-	set_transform(get_viewport().get_global_canvas_transform().affine_inverse())
-	plugin.forward_draw_over_canvas(get_viewport().get_global_canvas_transform(), self)
